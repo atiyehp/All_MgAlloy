84897d0d3485cf3113d998431c1ca5eab118ea1d
atiyehp <42898707+atiyehp@users.noreply.github.com>
2022-11-25 12:03:48 +0330

    Update BO_append.py

--- BO_append.py
+++ BO_append.py
@@ -1,5 +1,5 @@
 # todo: uncomment this?!
-import alloysHT
+import All_MgAlloy.alloysHT
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.gaussian_process import GaussianProcessRegressor
